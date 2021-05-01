b2597dba6520bf5eb048a87e600e3c6034e7215d
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:22:28 +0530

    Add project files.

---
+++ LearningStack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<String> pancakes = new Stack<string>();
+
+            pancakes.Push("First Pancake made");
+            pancakes.Push("Second Pancake made");
+            pancakes.Push("Third Pancake made");
+
+            foreach (var pancake in pancakes){
+                Console.WriteLine(pancake);
+                Console.WriteLine(pancakes.Peek());
+            }
+
+            Console.WriteLine(".................................");
+            Console.WriteLine(pancakes.Pop());
+            Console.WriteLine(pancakes.Peek());
+            Console.WriteLine(pancakes.Pop());
+            Console.WriteLine(pancakes.Peek());
+
+        }
+    }
+}
